994e327c0538ec089157edafbd5d552a7662d612
Tammer Barkouki <tammer.barkouki@gmail.com>
2018-03-12 21:16:09 -0700

    Start adding possessions to PlayGameActivity, comment out doc.addView(possessions) for now because it crashes

---
+++ app/src/main/java/edu/stanford/cs108/bunnyworld/ Document.java
@@ -0,0 +1,79 @@
+package edu.stanford.cs108.bunnyworld;
+
+import android.animation.LayoutTransition;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.os.Parcel;
+import android.os.Parcelable;
+import android.support.v4.view.PagerAdapter;
+import android.util.AttributeSet;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+import java.io.Serializable;
+
+// test test test
+
+/**
+ * Created by emohelw on 2/24/2018.
+ */
+
+public class Document extends LinearLayout {
+
+    private String gameName = "";
+    private String iconName = "";
+    private String gameType = "";
+
+    //public View possessions;
+    private LinearLayout.LayoutParams lpPossessions;
+
+    private LinearLayout.LayoutParams lpPages;
+
+    public Document(Context context) {
+        super(context);
+        this.setOrientation(LinearLayout.VERTICAL);
+        this.setWeightSum(1.0f);
+        this.setGravity(Gravity.BOTTOM);
+        //this.setBaselineAligned(false);
+        lpPossessions = new LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT, 0, 0.3f);
+        lpPages = new LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT,0, 0.7f);
+        this.setLayoutTransition(new LayoutTransition());  //regular animation enforced by the layout.
+        //possessions = new Possessions(context);
+        //possessions.setLayoutParams(lpPossessions);
+        //possessions.setVisibility(VISIBLE);
+        //this.addView(possessions);
+    }
+    public Document(Context context, String name) {
+        this(context);
+        this.gameName = name;
+
+    }
+    public Document(Context context, String name,String iconName,String gameType) {
+        this(context);
+        this.gameName = name;
+        this.iconName = iconName;
+        //this.gameType = gameType;
+
+    }
+
+    public Document(Context context, AttributeSet attrs) {
+        super(context, attrs, 0);
+    }
+
+    public Document(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+
+    //retrieve page and possessions layout
+    public LinearLayout.LayoutParams getLpPages() {
+        return lpPages;
+    }
+    public LinearLayout.LayoutParams getLpPossessions() {
+        return lpPossessions;
+    }
+
+
+}
